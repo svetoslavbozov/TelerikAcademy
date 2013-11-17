64e0d55dfe10f1dba64d4a7dfa654c2d1591b92d
svetoslavbozov <svetoslavbozov@abv.bg>
2013-11-17 11:22:46 +0200

    update

---
+++ 1.Programming/03.OOP/ExtentionMethods/01.ImplementSubstring/ImplementSubstring.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ImplementSubstring
+{
+    class ImplementSubstring
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder text = new StringBuilder("adwadwadwadawdawdadwawdawdawd");
+
+            StringBuilder result = text.Substring(0, 3);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 1.Programming/03.OOP/ExtentionMethods/06.PrintNumsDivisibleBy7and5/PrintNumsDivisibleBy7and5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.PrintNumsDivisibleBy7and5
+{
+    class PrintNumsDivisibleBy7and5
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
